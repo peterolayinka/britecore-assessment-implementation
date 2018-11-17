8581da6ee4e50918f36f4389ebbc006f90c82947
Peter Olayinka <peterolayinka97@gmail.com>
2018-11-17 21:29:09 +0100

    spin app using gunicorn

--- compose/docker-entrypoint.sh
+++ compose/docker-entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 flask db upgrade
 
-flask run
-
 # Start Gunicorn processes
-# exec gunicorn config.wsgi:application \
-#     --name quizboot_web \
-#     --bind 0.0.0.0:8000 \
-#     --workers 3 \
-#     --worker-class="egg:meinheld#gunicorn_worker" \
-#     --log-level=info \
-#     "$@"
+exec gunicorn config.wsgi:application \
+    --name quizboot_web \
+    --bind 0.0.0.0:8000 \
+    --workers 3 \
+    --worker-class="egg:meinheld#gunicorn_worker" \
+    --log-level=info \
+    "$@"
