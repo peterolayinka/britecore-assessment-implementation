0d04004fce608ef0ba4494721ba649e5cf8db64b
Peter Olayinka <peterolayinka97@gmail.com>
2018-11-17 21:34:32 +0100

    modified startup

--- compose/docker-entrypoint.sh
+++ compose/docker-entrypoint.sh
@@ -3,8 +3,8 @@ flask db migrate
 flask db upgrade
 
 # Start Gunicorn processes
-exec gunicorn config.wsgi:application \
-    --name quizboot_web \
+exec gunicorn config.wsgi:app \
+    --name britecore_web \
     --bind 0.0.0.0:8000 \
     --workers 3 \
     --log-level=info \
