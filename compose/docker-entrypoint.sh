928a736a03fdaf4e42bf908ccda1665506491c6c
Peter Olayinka <peterolayinka97@gmail.com>
2018-11-17 21:33:20 +0100

    modified startup

--- compose/docker-entrypoint.sh
+++ compose/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+flask db migrate
 flask db upgrade
 
 # Start Gunicorn processes
@@ -6,6 +7,5 @@ exec gunicorn config.wsgi:application \
     --name quizboot_web \
     --bind 0.0.0.0:8000 \
     --workers 3 \
-    --worker-class="egg:meinheld#gunicorn_worker" \
     --log-level=info \
     "$@"
