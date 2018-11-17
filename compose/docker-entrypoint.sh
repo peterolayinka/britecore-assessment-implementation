28ac58a9de82efb941027d1eb5751ba764843a6a
Peter Olayinka <peterolayinka97@gmail.com>
2018-11-17 22:07:50 +0100

    launch with default flak run

--- compose/docker-entrypoint.sh
+++ compose/docker-entrypoint.sh
@@ -2,10 +2,12 @@
 flask db migrate
 flask db upgrade
 
+flask run --port 5000 --host 0.0.0.0
+
 # Start Gunicorn processes
-exec gunicorn config.wsgi:app \
-    --name britecore_web \
-    --bind 0.0.0.0:8000 \
-    --workers 3 \
-    --log-level=info \
-    "$@"
+# exec gunicorn config.wsgi:app \
+#     --name britecore_web \
+#     --bind 0.0.0.0:8000 \
+#     --workers 3 \
+#     --log-level=info \
+#     "$@"
